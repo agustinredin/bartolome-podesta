fd44ef42868b55572890afeac1ea11959ee415a1
Ulises <u.ivalero@hotmail.com>
2025-03-14 20:04:01 -0300

    v0.1 add cards.jsx & itemlist.js

---
+++ src/components/ItemList.js
@@ -0,0 +1,29 @@
+const Products = [
+    {
+        id: 1,
+        nombre:"Jeringa",
+        img: "Insertar imagen"
+    },
+    {
+        id: 2,
+        nombre: "Cinta",
+        img: "Insertar imagen"
+    },
+    {
+        id: 3,
+        nombre: "Alcohol",
+        img: "Insertar imagen"
+    },
+    {
+        id: 4,
+        nombre: "Pastillas",
+        img: "Insertar imagen"
+    },
+    {
+        id: 5,
+        nombre: "Vendas",
+        img: "Insertar imagen"
+    }
+]
+
+export default Products
