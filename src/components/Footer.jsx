142874c9220f4b892da454804459c23cccaff352
Ulises <u.ivalero@hotmail.com>
2025-03-19 12:03:15 -0300

    v0.4 footer & map correction

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,86 +1,42 @@
 import "../App.css";
 import Map from "./ui/Map";
-import { FaFacebook, FaInstagram, FaXTwitter } from "react-icons/fa6";
+import Container from "./ui/Container";
+
 
 const Footer = () => {
-    // moví un par de archivos y configuré mejor la estructura del proyecto. fijate bien esto
-
-    // └───src
-    // │   App.css
-    // │   App.jsx
-    // │   main.jsx
-    // │
-    // ├───components
-    // │   │   Cards.jsx
-    // │   │   Footer.jsx
-    // │   │   Hero.jsx
-    // │   │   NavBar.jsx
-    // │   │
-    // │   └───ui
-    // │           Container.jsx
-    // │           Map.jsx
-    // │
-    // ├───lib
-    // │       ProductList.js
-    // │       utils.js
-    // │
-    // └───pages
-    //         AboutUs.jsx
-    //         Contact.jsx
-    //         FrequentlyAsked.jsx
-    //         Products.jsx
-
-    // /components/ui son elementos concretos, por ejemplo un mapa, o componentes reutilizables sin contenido, ej container
-    // /components son las secciones
-    // /pages son las paginas como si fuera /pages en un proyecto html nativo. el index es App.jsx
-    // /lib son archivos js que sirven (por ejemplo origen de datos en forma de array)
-    // configuré el app router y el .env
-    // vos preguntá
-
-
-
-    // que tenes que hacer aca en footer:
-    // 1 hacer que el area de la section sea toda, es decir, que no haya espacios en blanco a los costados
-    // 2 mapa un 15/20% mas chico (tanto w como h) y usando solo tailwind automaticamente responsive:
-    // // (si hay un bug visual no lo arregles con 95% 95%, fijate de reordenar las clases de css)
-    // // (y tambien no uses un array con 2 iframe, hace uno solo y de ultima yo arreglo el cambio de desktop a mobile)
-    // 3 la parte de abajo del footer con el mismo color que arriba y con una linea de separación, exactamente como en la referencia
-    // 4 reemplazarle la información lorem por la información real según la de la referencia 
-    // 5 sacarle los iconos de redes, segun entendí no tienen ninguna, aunque esten en la referencia
-    // 6 minimizar la cantidad de clases custom (-[valor]) asi despues es mas facil hacerlo responsive 
-    // 7 hacer dinamica la api key de map.jsx: que cargue del .env el valor MAPS_API_KEY
-
     return (
-        <footer className="flex justify-center">
-            <section className="w-[50vw] h-[65vh] bg-black text-white flex flex-col">
+        <footer className="flex justify-center bg-black pt-4 pb-4">
+            <Container className="w-[60vw] h-[65vh] text-white flex flex-col justify-center">
                 <div className="h-[90%] flex">
                     <div className="w-6/12 flex">
                         <div className="w-1/2 flex flex-col pt-[3rem] pl-1">
-                            <h3 className="pb-[1.3rem]">lorem ipsum</h3>
-                            <p className="pt-4 pb-[4rem]">{`Av.Random 3142 - C.A.B.A\nCp1000 - BSAS, Argentina`}</p>
-                            <h3 className="pb-[1.3rem]">lorem ipsum</h3>
-                            <p className="pt-4">{`Av.Random 3142 - C.A.B.A\nCp1000 - BSAS, Argentina`}</p>
+                            <h3 className="pb-[1.3rem]">Dirección</h3>
+                            <p className="text-sm pt-3 pb-[4rem]">{`Av.Gaona 3157 - C.A.B.A\nC1416DSG - Rep.Argentina`}</p>
+                            <h3 className="pb-[1.3rem]">Contacto</h3>
+                            <p className="text-sm">Mail: ba_podesta@arnet.com.ar</p>
+                            <p className="text-sm">Teléfono: +54 114583-8441</p>
+                            <p className="text-sm">4581-2849  4582-4086</p>
+
                         </div>
 
                         <div className="w-1/2 flex flex-col pt-[3rem] pl-1">
-                            <h3 className="pb-[1.3rem]">lorem ipsum</h3>
-                            <p className="pt-4 pb-[4rem]">{`Av.Random 3142 - C.A.B.A\nCp1000 - BSAS, Argentina`}</p>
-                            <h3 className="pb-[1.3rem]">lorem ipsum</h3>
-                            <p className="pt-4">{`Av.Random 3142 - C.A.B.A\nCp1000 - BSAS, Argentina`}</p></div>
+                            <h3 className="pb-[1.3rem]">Links</h3>
+                            <a className="text-sm" href="/about">Nosotros</a>
+                            <a className="text-sm" href="/products">Productos</a>
+                            <a className="text-sm pb-[4rem]" href="/faq">Preguntas frecuentes</a>
+                            <h3 className="pb-[1.3rem]">Empresa</h3>
+                            <p className="text-sm pt-4">Bartolomé Podesta</p>
+                            <p className="text-sm">Productos medicinales</p>
+                        </div>
                     </div>
                     <div className="w-7/12 flex flex-col justify-center items-center">
-                        {Map[0].smMap}
+                        {Map[0].googleMap}
                     </div>
                 </div>
-                <div className="h-[10%] flex items-center pl-1 bg-gray-700">
-                    <p className="flex flex-col w-1/2 text-sm">Copyright © 2012 - 2025 TermsFeed®. All rights reserved.</p>
-                    <nav className="flex w-1/2 flex-row justify-end pr-1">
-                        <a className="pr-1" href=""><FaFacebook/></a>
-                        <a className="pr-1" href=""><FaInstagram/></a>
-                        <a href=""><FaXTwitter/></a>
-                    </nav>
+                <div className="h-[10%] flex items-center pl-1 border-t-1 border-t-white/15 bg-black-700">
+                    <p className="flex flex-col w-1/2 text-sm">© Copyright 2020 Bartolomé Podesta - All Rights Reserved</p>
                 </div>
-            </section>
+            </Container>
         </footer>
     );
 }
--- src/components/ui/Map.jsx
+++ src/components/ui/Map.jsx
@@ -1,21 +1,13 @@
 const Map = [
     {
-        smMap: (
-            <iframe className="smMap"
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEIpgj38KyLFELm2bK9Y7krBkz1K-cMq8&q=place_id:ChIJ3RdS4fPJvJURhmJwk5B1_aI"
+        googleMap: (
+            <iframe className="w-[85%] h-[75%]"
+                src={`https://www.google.com/maps/embed/v1/place?key=${import.meta.env.VITE_MAPS_API_KEY}&q=place_id:ChIJ3RdS4fPJvJURhmJwk5B1_aI`}
                 allowFullScreen=""
                 loading="lazy"
                 referrerPolicy="no-referrer-when-downgrade"
             ></iframe>
-        ),
-        mdMap: (
-            <iframe className="mdMap"
-                src="https://www.google.com/maps/embed/v1/place?key=AIzaSyAEIpgj38KyLFELm2bK9Y7krBkz1K-cMq8&q=place_id:ChIJ3RdS4fPJvJURhmJwk5B1_aI"
-                allowFullScreen=""
-                loading="lazy"
-                referrerPolicy="no-referrer-when-downgrade"
-            ></iframe>
-        ),
+        )
     },
 ];
 
