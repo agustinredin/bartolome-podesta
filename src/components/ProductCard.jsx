e9fcfc99403f17cd8dacbe0130a140e6d2b0163f
Ulises <u.ivalero@hotmail.com>
2025-04-02 20:59:32 -0300

    v0.9 [ADD] products.jsx

--- src/components/ProductCard.jsx
+++ src/components/ProductCard.jsx
@@ -1,7 +1,7 @@
 function ProductCard({ title, description, image, className }) {
   return (
     <div className={`flex-3/4 p-2 ${className}`}>
-      <div className="flex flex-col bg-white rounded-lg shadow-md overflow-hidden border border-gray-100">
+      <div className="flex flex-col p-4 bg-[#fff] rounded-lg shadow-md overflow-hidden border border-gray-100">
         <div className="relative h-64">
           <img
             src={image}
@@ -9,8 +9,8 @@ function ProductCard({ title, description, image, className }) {
             alt={title}
           />
         </div>
-        <div className="p-4 flex flex-col gap-2">
-          <h4 className="text-lg font-semibold text-gray-900">{title}</h4>
+        <div className="py-4 flex flex-col gap-2">
+          <h4 className="text-lg font-semibold text-gray-900 h-16">{title}</h4>
           <p className="text-sm text-gray-600 line-clamp-3">{description}</p>
         </div>
       </div>
--- src/components/ui/Container.jsx
+++ src/components/ui/Container.jsx
@@ -4,6 +4,8 @@ import { cn } from '../../lib/utils';
 const Container = ({className, children, ...props}) => {
   return (
     <section
+        /*Modifiqué el w-[85vw] y max-w-screen-lg para que las cards no tengan 
+        el w limitado y las imagenes puedan verse mejor */
       className={cn("w-[85vw] max-w-screen-lg mx-auto my-16", className)}
       {...props}
     >
